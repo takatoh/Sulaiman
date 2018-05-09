c7e9e2e6123a32e315fc66f5a546e1c3884d6e2a
takatoh <takatoh.m@gmail.com>
2018-05-09 14:21:06 +0900

    server.go: Use db and Handler struct.

--- server.go
+++ server.go
@@ -4,22 +4,34 @@ import (
 //	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 
 	"github.com/takatoh/sulaiman/handler"
+	"github.com/takatoh/sulaiman/data"
 )
 
 func main() {
-	e := echo.New()
+	db, err := gorm.Open("sqlite3", "sulaiman.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	e.GET("/", handler.IndexGet)
+	db.AutoMigrate(&data.Photo{})
+
+	e := echo.New()
+	h := handler.New(db)
+
+	e.GET("/", h.IndexGet)
 
 	e.Static("/css", "static/css")
 	e.Static("/js", "static/js")
 	e.Static("/img", "photos/img")
 	e.Static("/thumb", "photos/thumb")
 
-	e.GET("/list/:page", handler.ListGet)
-	e.POST("/upload", handler.UploadPost)
+	e.GET("/list/:page", h.ListGet)
+	e.POST("/upload", h.UploadPost)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
