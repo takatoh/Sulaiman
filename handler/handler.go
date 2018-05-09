08d2974e1f1adc32610c5332212cb430255dc454
takatoh <takatoh.m@gmail.com>
2018-05-09 11:14:02 +0900

    handler/handler.go(UploadPost): New.

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"os"
+	"io"
+	"net/http"
+
 	"github.com/labstack/echo"
 )
 
@@ -11,4 +15,39 @@ func IndexGet(c echo.Context) error {
 func ListGet(c echo.Context) error {
 	jsonFile := "list" + c.Param("page") + ".json"
 		return c.File(jsonFile)
+}
+
+func UploadPost(c echo.Context) error {
+	file, _ := c.FormFile("file")
+	src, _ := file.Open()
+	defer src.Close()
+
+	dst, _ := os.Create("photos/img/img101.jpg")
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	res := UploadResponse{
+		Status: "OK",
+		Photo: Photo{
+			ID: 101,
+			Url: "http://localhost:1323/img/img101.jpg",
+			Thumb: "http://localhost:1323/thumb/thumb101.jpg",
+		},
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
+
+type UploadResponse struct {
+	Status string `json:"status"`
+	Photo  Photo  `json:"photo"`
+}
+
+type Photo struct {
+	ID    int    `json:"id"`
+	Url   string `json:"url"`
+	Thumb string `json:"thumb"`
 }
