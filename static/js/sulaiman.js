d40067fa4617bbd3bc1c0065356409b0fb1913bf
takatoh <takatoh.m@gmail.com>
2018-05-09 12:50:43 +0900

    static/js/sulaiman.js: New.

---
+++ static/js/sulaiman.js
@@ -0,0 +1,51 @@
+let app;
+let photoList;
+$(function() {
+  let next_url = $("#next_link").attr("href");
+  $.ajax({
+    type: "GET",
+    url: next_url,
+    dataType: "json"
+  }).done(function(response) {
+    photoList = response.photos;
+    app = new Vue({
+      el: "#content",
+      data: {
+        photoList: photoList
+      }
+    });
+    $("#next_link").attr("href", response.next);
+  });
+
+  $("body").on("click", "#next_link", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let next_url = $("#next_link").attr("href");
+    $.ajax({
+      type: "GET",
+      url: next_url,
+      dataType: "json"
+    }).done(function(response) {
+      response.photos.forEach(function(v) {
+        photoList.push(v);
+      });
+      $("#next_link").attr("href", response.next);
+    });
+  });
+
+  $("#upload_button").on("click", function(event) {
+    let fd = new FormData($("#upload_form").get(0));
+    $.ajax({
+      url: "http://localhost:1323/upload",
+      type: "POST",
+      data: fd,
+      contentType: false,
+      processData: false,
+      dataType: "json"
+    }).done(function(response) {
+      $("input[name=file]").val("");
+      $("input[name=key]").val("");
+      photoList.unshift(response.photo);
+    });
+  });
+});
