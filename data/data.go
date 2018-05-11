858bce4d447dfddf018fe267d71c6b7b0ea111dc
takatoh <takatoh.m@gmail.com>
2018-05-11 11:37:32 +0900

    data/data.go: Add Config struct.

--- data/data.go
+++ data/data.go
@@ -10,3 +10,9 @@ type Photo struct {
 	ThumbPath string
 	DeleteKey string
 }
+
+type Config struct {
+	SiteName string `json:"site_name"`
+	HostName string `json:"host_name"`
+	Port     int    `json:"port"`
+}
