158275f1920576a56a586a336536802e9bc3459f
takatoh <takatoh.m@gmail.com>
2018-05-18 12:52:08 +0900

    static/js/sulaiman.js: Remove deleted-photo from list.

--- static/js/sulaiman.js
+++ static/js/sulaiman.js
@@ -89,6 +89,12 @@ $(function() {
       dataType: "json"
     }).done(function(response) {
       if (response.status == "OK") {
+        p.find("input[name=key]").val("");
+        photoList.some(function(v, i) {
+          if (v.id == response.photo_id) {
+            photoList.splice(i, 1);
+          }
+        });
         alert("Deleted: " + response.photo_id);
       } else {
         p.find("input[name=key]").val("");
