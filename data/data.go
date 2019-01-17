9453c75548710e79a6cde50cea3211fd37571fce
takatoh <takatoh.m@gmail.com>
2019-01-17 16:17:05 +0900

    data/data.go: Add field 'MaxPhotoCount' to Config struct.

--- data/data.go
+++ data/data.go
@@ -12,8 +12,9 @@ type Photo struct {
 }
 
 type Config struct {
-	SiteName string `json:"site_name"`
-	HostName string `json:"host_name"`
-	Port     int    `json:"port"`
-	PhotoDir string `json:"photo_dir"`
+	SiteName      string `json:"site_name"`
+	HostName      string `json:"host_name"`
+	Port          int    `json:"port"`
+	PhotoDir      string `json:"photo_dir"`
+	MaxPhotoCount int    `json:"max_photo_count"`
 }
