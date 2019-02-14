09927f5ab8336fe388a2b8d055cd7fad2136cf13
takatoh <takatoh.m@gmail.com>
2019-02-14 13:39:13 +0900

    static/js/sulaiman.js: Cleaning.

--- static/js/sulaiman.js
+++ static/js/sulaiman.js
@@ -48,9 +48,7 @@ $(function() {
       processData: false,
       dataType: "json"
     }).done(function(response) {
-//      alert(response.status);
       if (response.status == "OK") {
-//        p.find("input[name=key]").val("");
         clear_delete_vals();
         photoList.some(function(v, i) {
           if (v.id == response.photo_id) {
@@ -168,32 +166,4 @@ $(function() {
   });
 
   $("#upload_button").on("click", upload);
-
-/*
-  $("body").on("click", ".delete_button", function(event) {
-    let p = $(this).parent();
-    let fd = new FormData(p.get(0));
-    $.ajax({
-      url: "/delete",
-      type: "DELETE",
-      data: fd,
-      contentType: false,
-      processData: false,
-      dataType: "json"
-    }).done(function(response) {
-      if (response.status == "OK") {
-        p.find("input[name=key]").val("");
-        photoList.some(function(v, i) {
-          if (v.id == response.photo_id) {
-            photoList.splice(i, 1);
-          }
-        });
-        alert("Deleted: " + response.photo_id);
-      } else {
-        p.find("input[name=key]").val("");
-        alert("Error! CAN'T delete: " + response.photo_id);
-      }
-    });
-  });
-*/
 });
